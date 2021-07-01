957ac4ac1f5917c936ed3233f938be7f18e15932
Ibrahim <ibrahimzerlin@hotmail.com>
2021-07-01 11:13:22 -0500

    all done

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -32,6 +33,36 @@ export const ADD_FRIEND = gql`
       friendCount
       friends {
         _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
         username
       }
     }
